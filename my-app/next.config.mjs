ab614e13ecfd914ebdee0e8d6c00ef1b24e61dde
tee229 <78217879+tee229@users.noreply.github.com>
2024-08-31 01:07:59 +0000

    add startplan

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // 启用react严格模式(可选)
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        cpus: 8,
+    },
+};
 
 export default nextConfig;
