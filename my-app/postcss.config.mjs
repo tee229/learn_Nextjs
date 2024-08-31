59dd16303da93e49215c31e649e6b885d1120c7c
tee229 <78217879+tee229@users.noreply.github.com>
2024-08-31 01:11:46 +0000

    add postcss config

--- my-app/postcss.config.mjs
+++ my-app/postcss.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
+    'postcss-nesting': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
+    autoprefixer: {},
+    'postcss-mixins': {},
   },
 };
 
