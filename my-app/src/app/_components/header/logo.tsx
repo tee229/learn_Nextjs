1113a8706a81749912446858efd9d078ee3820d8
tee229 <78217879+tee229@users.noreply.github.com>
2024-09-06 15:15:04 +0000

    add logo tee229

--- my-app/src/app/_components/header/logo.tsx
+++ my-app/src/app/_components/header/logo.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Avatar from './avatar.svg';
+// import Avatar from './avatar.svg';
+import Tee from './tee229.png';
 
 import $styles from './logo.module.css';
 
 export const HeaderLogo = () => (
     <Link href="/" className={$styles.link}>
         <Image
-            src={Avatar}
+            // src={Avatar}
+            src={Tee}
             alt="avatar logo"
             sizes="100vw"
             style={{
